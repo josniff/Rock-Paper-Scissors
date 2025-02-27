c565b76db511f999e947cfc15ff564a64aa3bc7a
josniff <186874932+josniff@users.noreply.github.com>
2025-02-27 16:33:26 -0600

    create the getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hello, World!");
+
+// getComputerChoice should randomly return the string value "rock", "paper", or "scissors"
+// math.random returns a number greater than or equal to 0 but less than one
+//  
+
+function getComputerChoice() {
+ return Math.floor(Math.random() * 3 + 1);
+} if (getComputerChoice() === 1) {
+    console.log("Rock");
+} else if (getComputerChoice() === 2) {
+    console.log("Paper");
+} else if (getComputerChoice() === 3) {
+    console.log("Scissors");
+}
